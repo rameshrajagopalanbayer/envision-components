b36fc66b3df77134e003b3f4bb03b3bab59b080c
rkraja <ramesh.rajagopalan@bayer.com>
2024-07-22 18:34:11 -0500

    one export works

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(),
+        dts({include: ['lib']})],
+    build: {
+        rollupOptions: {
+            external: ['react', 'react/jsx-runtime'],
+        },
+        copyPublicDir: false,
+        lib: {
+            entry: resolve(__dirname, 'lib/main.ts'),
+            formats: ['es']
+        }
+    }
 })
